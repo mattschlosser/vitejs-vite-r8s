2f771f9c0be4a5a675a65866e3ad927c302c1585
Matt Schlosser <mattschlosser@users.noreply.github.com>
2023-02-25 10:08:15 -0700

    fix: show menu on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
-import AppToolbar from './components/AppToolbar.vue';
+import { useAuth } from './hooks/useAuth';
+import { ref, type Ref } from "vue"
+const drawer: Ref<boolean | null>  = ref(null)
+const { hasLoggedIn, logout } = useAuth()
 </script>
 <template>
   <v-app>
-    <AppToolbar />
-    <VNavigationDrawer expand-on-hover rail>
+    <v-app-bar flat color="#ffeecc">
+        <VAppBarNavIcon @click.stop="drawer = !drawer" />
+        <v-toolbar-title >
+            Roomm8s
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn v-if="hasLoggedIn" icon @click="logout">
+          <v-icon>mdi-export</v-icon>
+        </v-btn>
+    </v-app-bar>
+    <VNavigationDrawer v-model="drawer" expand-on-hover rail>
       <v-list>
         <v-list-item prepend-icon="mdi-home" to="/households">
           Households
